5ea010eaf6678d60db4a55929392e2cec2acacbc
imran <85463371+imranrszd@users.noreply.github.com>
2023-12-29 02:33:02 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 
-$conn = mysqli_connect('localhost','root','','vtech') or die('connection failed');
+$conn = mysqli_connect('localhost','id21725116_root','Venus01#','id21725116_vtech') or die('connection failed');
 
 ?>
